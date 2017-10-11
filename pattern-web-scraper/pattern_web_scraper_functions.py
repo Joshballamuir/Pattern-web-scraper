86bf40e92933ace1ceae3df8f42cde8e4b7ef4d8
joshballamuir <joshballamuir@gmail.com>
2017-10-11 10:46:52 +0100

    First github edit

---
+++ pattern-web-scraper/pattern_web_scraper_functions.py
@@ -0,0 +1,103 @@
+import collections
+import re
+import requests
+import ssl
+import sys
+
+session = requests.Session()
+session.max_redirects = 20
+
+non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
+non_letters = re.compile('[^a-zA-Z]')
+
+class MyAdapter(requests.adapters.HTTPAdapter):
+    def init_poolmanager(self, connections, maxsize, block=False):
+        self.poolmanager = PoolManager(
+            num_pools=connections,
+            maxsize=maxsize,
+            block=block,
+            ssl_version=ssl.PROTOCOL_TLSv1
+            )
+
+def slug(slug_item):
+    return(re.sub(r' ','+',re.sub(r'&','and',slug_item)).lower())
+#converts item to name to a valid google search format
+def gen_name(new_name):
+    #from pattern_web_scraper_executable import blacklist
+    from pattern_web_scraper_test_suite import blacklist
+    first_name = new_name[0]
+    try:
+        last_name = new_name[1]
+    except IndexError:
+        return('Not found')
+    full_name = first_name+' '+last_name
+    for i in blacklist:
+        if i in full_name:
+            return('Not found')
+        else:
+            continue
+    return(first_name,last_name,full_name)
+#generates list of names from descriptions of linkedin search results checked against blacklist
+###TESTED FUNCTIONS###
+
+def linkedin_google_search(employee):
+    data = ''
+    if employee in synonyms_1:
+        synonyms = synonyms_1
+    elif employee in synonyms_2:
+        synonyms = synonyms_2
+    elif employee in synonyms_3:
+        synonyms = synonyms_3
+    for i in synonyms:
+        urls.append(url+i+'+linkedin')
+    return(urls)
+#generates start urls
+def gen_html_text(urls,idword):
+    for i in urls:
+        session.mount(site,MyAdapter())
+        try:
+            html_dump = html_dump + session.get(i,allow_redirects=False).text
+        except(
+            requests.exceptions.SSLError,requests.exceptions.ConnectionError,
+            requests.packages.urllib3.exceptions.NewConnectionError,
+            requests.packages.urllib3.exceptions.MaxRetryError
+            ) as e:
+                continue
+    return(html_dump)
+#generates the html text
+def html_dump_prep(html_dump):
+    re.sub(non_letters.sub(' ',html_dump).translate(non_bmp_map).lower())
+    for i in prep:
+        html_dump = re.sub(r+i,' ',html_dump)
+    return(html_dump)
+#preps the html dump for the finding function
+def word_finder(html_dump):
+    found = []
+    before_idword,idword,after_idword = html_dump.partition(idword)
+    loop = 1
+    while loop == 1:
+        found.append(after_idword2.split()[1])
+        try:
+            before_idword,idword,after_idword = after_idword.partition(idword)
+        except ValueError:
+            return(found)
+        #found = urls for 3dcache
+        #find new idword to replace '|+<b>' so it works for AFTER keyword, rather than before - also find removables and put into a list!
+#generates a list of words from data by iterating through a found keyword
+def name_compilation(first_name_list,html_dump):
+    for i in first_name_list:
+        #consider adding points for last_name
+        if i in html_dump:
+            final_names.append(list(re.findall('\\b'+i+'\\b',html_dump)))#not showing all names?#repeat for surname?
+    final_names = (Counter(final_names).most_common(2))
+    return(final_names)
+#compiles list of names found and narrows it down to 2 final names
+def name_final(final_names):
+    try:
+        for i in final_names:
+            if i in full_name_list:
+                print(i+[i]).title()
+    except IndexError:
+        print('Not found')
+#finalises and reformats 2 names to be returned
+###UNTESTED FUNCTIONS###
---
+++ pattern-web-scraper/pattern_web_scraper_test_suite.py
@@ -0,0 +1,55 @@
+import unittest
+import pattern_web_scraper_functions
+
+#import from pattern_web_scraper_executable:
+blacklist = [
+         '&','#',':',';','(',')','group','technologies','direct','careers',
+         'tooshort','profiles', 'accountant', 'jobs', 'united', 'kingdom',
+         'international', 'plc','lp', 'linkedin','ltd','limited','...',
+         '/script','/body','director','finance','cfo','chief','financial',
+         'officer','controller','accounts','manager','management','fd',
+         'presidential','articles','salaries','telecom','solutions',
+         'area','the','head','of','uk'
+         ]
+         #blacklist for words frequently appearing where names are found in search results
+first_name_list = []
+last_name_list = []
+full_name_list = []
+
+class pattern_web_scraper_functions_TestCase(unittest.TestCase):
+    """Tests for `new_name_finder_functions.py`."""
+#slug
+    def test_is_company_name_lower(self):
+        """Does it convert to lower case?"""
+        self.assertEqual('hello',pattern_web_scraper_functions.slug('hELlO'))
+    def test_is_company_name_spaced(self):
+        """Does it replace ' ' with '+'?"""
+        self.assertEqual('h+e+l+l+o',pattern_web_scraper_functions.slug('h e l l o'))
+    def test_is_company_name_and(self):
+        """Does it replace '&'' with 'and'?"""
+        self.assertEqual('and',pattern_web_scraper_functions.slug('&'))
+    def test_is_company_name_valid(self):
+        """Does it validate company name?"""
+        self.assertEqual('hello+and++++bonjour',pattern_web_scraper_functions.slug('hELlo &    BONjOUR'))
+#gen_name
+    def test_is_full_name_output(self):
+        """Does it output first_name, last_name and full_name from a list?"""
+        self.assertEqual(('josh','balla-muir','josh balla-muir'),pattern_web_scraper_functions.gen_name(['josh','balla-muir','hello','fvfvrvfrd']))
+    def test_last_tooshort(self):
+        """Does it identify when word is too short?"""
+        self.assertEqual('Not found',pattern_web_scraper_functions.gen_name(['balla-muir']))
+    def test_blacklist(self):
+        """Does it identify when a blacklisted word appears?"""
+        self.assertEqual('Not found',pattern_web_scraper_functions.gen_name(['josh','profiles']))
+
+
+#linkedin_google_search
+#gen_html_text
+#html_dump_prep
+#word_finder
+#name_compilation
+#name_final
+
+if __name__ == '__main__':
+    unittest.main()
+#test, prove working, then upload to github. future changes may be made in the future.
