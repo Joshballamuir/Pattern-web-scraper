a93d6b9592c5a99886e3eba8aa5de9644688e3d3
joshballamuir <joshballamuir@gmail.com>
2017-10-26 11:55:40 +0100

    start of a future endeavour!

---
+++ pattern_locator_alpha.py
@@ -0,0 +1,12 @@
+import collections
+
+def pattern_word_locator(html_dump):
+    examples = []
+    html_dump = re.sub(r'+',' ',re.sub(r',','',html_dump)).lower()
+    html_dump = html_dump.split()
+    loop = 1
+    #research 3d arrays
+    for i in html_dump:
+        words = words + (for i in examples)[-1],(for i in examples)[1]
+    possible_pattern_word = (words).most_common(50))
+    return(possible_pattern_word)
