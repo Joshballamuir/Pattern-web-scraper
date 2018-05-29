6cecdaec7b09c4364c9e42edd9915b2fd7de79f2
joshballamuir <joshballamuir@gmail.com>
2018-05-29 22:34:06 +0100

    further tidying up

--- pattern-web-scraper/pattern_web_scraper_functions.py
+++ pattern-web-scraper/pattern_web_scraper_functions.py
@@ -1,30 +1,18 @@
-import collections
-import re
-import requests
-import ssl
-import sys
-from pattern_web_scraper_test_suite import blacklist,synonyms_1,synonyms_2,synonyms_3,url,idword
-#from pattern_web_scraper_executable import blacklist,synonyms_1,synonyms_2,synonyms_3,url,idword
 
-session = requests.Session()
-session.max_redirects = 20
+import pattern_web_scraper_test_suite
 
-non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
-non_letters = re.compile('[^a-zA-Z]')
+def linkedin_google_search(employee):
+    url_list = []
+    if employee in synonyms:
+    for i in synonyms:
+        url_list.append(url+i+'+linkedin')
+    return(url_list)
+    #generates start urls
 
-class MyAdapter(requests.adapters.HTTPAdapter):
-    def init_poolmanager(self, connections, maxsize, block=False):
-        self.poolmanager = PoolManager(
-            num_pools=connections,
-            maxsize=maxsize,
-            block=block,
-            ssl_version=ssl.PROTOCOL_TLSv1
-            )
-
-###TESTED FUNCTIONS###:
 def slug(slug_item):
     return(re.sub(r' ','+',re.sub(r'&','and',slug_item)).lower())
-#converts item to name to a valid google search format
+    #converts item to name to a valid google search format
+
 def gen_name(new_name):
     first_name = new_name[0]
     try:
@@ -38,27 +26,15 @@ def gen_name(new_name):
         else:
             continue
     return(first_name,last_name,full_name)
-#generates list of names from descriptions of linkedin search results checked against blacklist
-def linkedin_google_search(employee):
-    url_list = []
-    if employee in synonyms_1:
-        synonyms = synonyms_1
-    elif employee in synonyms_2:
-        synonyms = synonyms_2
-    elif employee in synonyms_3:
-        synonyms = synonyms_3
-    for i in synonyms:
-        url_list.append(url+i+'+linkedin')
-    return(url_list)
-#generates start urls
+    #generates list of names from descriptions of linkedin search results checked against blacklist
 
-###UNTESTED FUNCTIONS###:
 def html_dump_prep(html_dump):
     re.sub(non_letters.sub(' ',html_dump).translate(non_bmp_map).lower())
     for i in prep:
         html_dump = re.sub(r+i,' ',html_dump)
     return(html_dump)
-#preps the html dump for the finding function
+    #preps the html dump for the finding function
+
 def word_finder(html_dump):
     found = []
     before_idword,idword,after_idword = html_dump.partition(idword)
@@ -71,7 +47,8 @@ def word_finder(html_dump):
             return(found)
         #found = urls for 3dcache
         #find new idword to replace '|+<b>' so it works for AFTER keyword, rather than before - also find removables and put into a list!
-#generates a list of words from data by iterating through a found keyword
+        #generates a list of words from data by iterating through a found keyword
+
 def name_compilation(first_name_list,html_dump):
     for i in first_name_list:
         #consider adding points for last_name
@@ -79,7 +56,8 @@ def name_compilation(first_name_list,html_dump):
             final_names.append(list(re.findall('\\b'+i+'\\b',html_dump)))#not showing all names?#repeat for surname?
     final_names = (Counter(final_names).most_common(2))
     return(final_names)
-#compiles list of names found and narrows it down to 2 final names
+    #compiles list of names found and narrows it down to 2 final names
+
 def name_final(final_names):
     try:
         for i in final_names:
@@ -87,7 +65,8 @@ def name_final(final_names):
                 print(i+[i]).title()
     except IndexError:
         print('Not found')
-#finalises and reformats 2 names to be returned
+        #finalises and reformats 2 names to be returned
+
 def gen_html_text(url_list,idword):
     for i in url_list:
         session.mount(site,MyAdapter())
@@ -100,4 +79,4 @@ def gen_html_text(url_list,idword):
             ) as e:
                 continue
     return(html_dump)
-#generates the html text
+    #generates the html text
