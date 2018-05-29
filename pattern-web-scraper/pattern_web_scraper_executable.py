21a3d14cc7d43fd5241670c6c70cd2dc02b35c46
joshballamuir <joshballamuir@gmail.com>
2018-05-29 22:23:03 +0100

    cleaning up executable

--- pattern-web-scraper/pattern_web_scraper_executable.py
+++ pattern-web-scraper/pattern_web_scraper_executable.py
@@ -21,18 +21,10 @@ google_page1 = ('https://www.google.co.uk/search?q='+subject)
 google_page2 = google_page1+'+&start=10'
 google_page3 = google_page1+'+&start=20'
 
-slug(googlelinkedinurlsetc) #format url
 
-wordlist_generator(
-    new_name_finder_functions.format_for_keyword(new_name_finder_functions.linkedin_google_search)).split() # #generate words
 
-word_final(
-    name_compilation(site_finder)
-    ) #determine best match
 
-first_name_list.append(first_name)
-last_name_list.append(last_name)
-full_name_list.append(full_name) #filter best match
+
 
 print(director1)
 print(director2) #output result
