8d4cce7d25c632e96fbff7adae518a5865d985fa
joshballamuir <joshballamuir@gmail.com>
2018-06-18 23:11:58 +0100

    working on test

--- pattern_web_scraper_executable.py
+++ pattern_web_scraper_executable.py
@@ -12,21 +12,25 @@ google_pattern1 = [
 linkedin_pattern = '|+'
 linkedin_pattern1 = '3Dcache'
 #linkedin pattern words to sift through data
+#automate pattern detection
+
 
 subject = input("Enter subject (e.g company name) ")
 synonyms = input("Enter location of your synonyms csv file ")
 blacklist = subject+synonyms+input("Enter blacklist csv ")
+#synonym lists to be plugged into programme at user's request. Soon automate with thesaurus programme
+
 
 google_page1 = ('https://www.google.co.uk/search?q='+subject)
 google_page2 = google_page1+'+&start=10'
 google_page3 = google_page1+'+&start=20'
 
 
-
 #print(director1)
 #print(director2) #output result
 
+
+
+
 #AIM TO MAKE ALL VARIABLES DISPOSABLE!
 #integrate into emails using files and dependencies as have began to here!
-#synonym lists to be plugged into programme at user's request. Soon automate with thesaurus programme
-#automate pattern detection
--- pattern_web_scraper_test_suite.py
+++ pattern_web_scraper_test_suite.py
@@ -6,6 +6,7 @@ subject = "mcdonalds"
 synonyms = "/Users/josh/josh_github/Pattern-web-scraper/Employee_position_lists/cfo.csv"
 blacklist = subject+synonyms+"/Users/josh/josh_github/Pattern-web-scraper/Employee_position_lists/position_blacklist.csv"
 
+name_list = ['josh','balla-muir','hello','fvfvrvfrd']
 
 class pattern_web_scraper_functions_TestCase(unittest.TestCase):
     """Tests for `new_name_finder_functions.py`."""
@@ -31,7 +32,7 @@ class pattern_web_scraper_functions_TestCase(unittest.TestCase):
 #gen_name:
     def test_is_full_name_output(self):
         """Does it output first_name, last_name and full_name from a list?"""
-        self.assertEqual(('josh','balla-muir','josh balla-muir'),pattern_web_scraper_functions.gen_name(['josh','balla-muir','hello','fvfvrvfrd']))
+        self.assertEqual('josh balla-muir josh balla-muir',pattern_web_scraper_functions.gen_name(name_list))
     def test_last_tooshort(self):
         """Does it identify when word is too short?"""
         self.assertEqual('Not found',pattern_web_scraper_functions.gen_name(['balla-muir']))
@@ -42,27 +43,27 @@ class pattern_web_scraper_functions_TestCase(unittest.TestCase):
 
 #UNTESTED:
 #linkedin_google_search:
-    def test_is_synonyms_1_selected(self):
-        """Does it select the correct list?"""
-        self.assertEqual([
-            'https://www.google.co.uk/search?q=testchief executive officer+linkedin',
-            'https://www.google.co.uk/search?q=testceo+linkedin',
-            'https://www.google.co.uk/search?q=testdirector+linkedin',
-            'https://www.google.co.uk/search?q=testhead of company+linkedin',
-            'https://www.google.co.uk/search?q=testchairman+linkedin',
-            'https://www.google.co.uk/search?q=testfounder+linkedin'
-                ],pattern_web_scraper_functions.linkedin_google_search('ceo'))
+#    def test_is_synonyms_1_selected(self):
+        #"""Does it select the correct list?"""
+        #self.assertEqual([
+        #    'https://www.google.co.uk/search?q=testchief executive officer+linkedin',
+        #    'https://www.google.co.uk/search?q=testceo+linkedin',
+        #    'https://www.google.co.uk/search?q=testdirector+linkedin',
+        #    'https://www.google.co.uk/search?q=testhead of company+linkedin',
+        #    'https://www.google.co.uk/search?q=testchairman+linkedin',
+        #    'https://www.google.co.uk/search?q=testfounder+linkedin'
+        #        ],pattern_web_scraper_functions.linkedin_google_search('ceo'))
 
-    def test_is_synonyms_1_selected_alternate(self):
-        """Does it select the correct list?"""
-        self.assertEqual([
-            'https://www.google.co.uk/search?q=testchief executive officer+linkedin',
-            'https://www.google.co.uk/search?q=testceo+linkedin',
-            'https://www.google.co.uk/search?q=testdirector+linkedin',
-            'https://www.google.co.uk/search?q=testhead of company+linkedin',
-            'https://www.google.co.uk/search?q=testchairman+linkedin',
-            'https://www.google.co.uk/search?q=testfounder+linkedin'
-                ],pattern_web_scraper_functions.linkedin_google_search('chairman'))
+#    def test_is_synonyms_1_selected_alternate(self):
+    #    """Does it select the correct list?"""
+    #    self.assertEqual([
+    #        'https://www.google.co.uk/search?q=testchief executive officer+linkedin',
+    #        'https://www.google.co.uk/search?q=testceo+linkedin',
+    #        'https://www.google.co.uk/search?q=testdirector+linkedin',
+    #        'https://www.google.co.uk/search?q=testhead of company+linkedin',
+    #        'https://www.google.co.uk/search?q=testchairman+linkedin',
+    #        'https://www.google.co.uk/search?q=testfounder+linkedin'
+    #            ],pattern_web_scraper_functions.linkedin_google_search('chairman'))
 
 #html_dump_prep
 #word_finder
